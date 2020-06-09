806363c335cd65e2860e0f4f2cd84a49864c34ce
Mohd Fasihullah <mfasihullah1994@gmail.com>
2020-06-09 20:53:03 +0530

    Initial commot

---
+++ springWithMicroServices/src/main/java/com/faiz/practice/SpringWithMicroServicesApplication.java
@@ -0,0 +1,13 @@
+package com.faiz.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithMicroServicesApplication.class, args);
+	}
+
+}
---
+++ springWithMicroServices/src/main/java/com/faiz/practice/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.faiz.practice.controller;
+
+public class UserController {
+
+}
